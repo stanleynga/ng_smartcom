3e5a71bc4a88f358ac3b636b781bcd557969171a
Stanley Nga <stanleynga@hotmail.com>
2019-10-07 09:09:33 +0800

    Install smartcomref with material, ngrx, firebase

---
+++ SmartCom/SmartComRef/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCXXPkwjRWgpg63S8LNnfao-1ODv18OzyM",
+    authDomain: "fire01-28286.firebaseapp.com",
+    databaseURL: "https://fire01-28286.firebaseio.com",
+    projectId: "fire01-28286",
+    storageBucket: "",
+    messagingSenderId: "417051932271",
+    appId: "1:417051932271:web:96d4b60d4cc33d840f3f9a",
+    measurementId: "G-VELJZZDJE2"
+  }
+};
---
+++ SmartCom/SmartComRef/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCXXPkwjRWgpg63S8LNnfao-1ODv18OzyM",
+    authDomain: "fire01-28286.firebaseapp.com",
+    databaseURL: "https://fire01-28286.firebaseio.com",
+    projectId: "fire01-28286",
+    storageBucket: "",
+    messagingSenderId: "417051932271",
+    appId: "1:417051932271:web:96d4b60d4cc33d840f3f9a",
+    measurementId: "G-VELJZZDJE2"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
